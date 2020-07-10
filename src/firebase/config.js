6ca284b6593685ada7744a334e25ab8d90eac65c
Artem <artem.oleinikov.web@gmail.com>
2020-07-10 19:12:29 +0300

    Initialize project using Create React App

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtxLb0RQnGbKIRyLTI-xpJD9a4YojAgG0",
+  authDomain: "hillel-auth.firebaseapp.com",
+  databaseURL: "https://hillel-auth.firebaseio.com",
+  projectId: "hillel-auth",
+  storageBucket: "hillel-auth.appspot.com",
+  messagingSenderId: "199603899316",
+  appId: "1:199603899316:web:54d370ef07a04df8d640e1"
+};
